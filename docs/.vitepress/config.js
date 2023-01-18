25ead151516d0a44221f04f5f3d05c4dfcb73235
luzilin <744883907@qq.com>
2023-01-18 14:42:18 +0800

    docs: 添加文档

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,39 @@
 export default {
-    title: '喵队仿掘金官网文档',
-    description: '喵队仿掘金官网文档',
-    base: "/bar/",
-  }
+  title: "喵队仿掘金官网文档",
+  description: "喵队仿掘金官网文档",
+  base: "/miaoNuggetsWebsiteDocs/",
+  themeConfig: {
+    nav: [
+      { text: '仿掘金项目', link: 'https://github.com/miaomiaoTeam/miaoNuggetsWebsite' }
+    ],
+    sidebar: {
+      "/": [
+        {
+          text: "结构",
+          // 是否可收缩
+          collapsible: true,
+          // 是否默认展开
+          collapsed: false,
+          items: [
+            {
+              text: "项目结构",
+              link: "/structures/project-structrue.md",
+            },
+            { text: "API接口", link: "/structures/api.md" },
+          ],
+        },
+        {
+          text: "规范",
+          // 是否可收缩
+          collapsible: true,
+          // 是否默认展开
+          collapsed: false,
+          items: [
+            { text: "项目命名规范", link: "/standards/name.md" },
+            { text: "Git提交规范", link: "/standards/git-commit.md" },
+          ],
+        },
+      ],
+    },
+  },
+};
