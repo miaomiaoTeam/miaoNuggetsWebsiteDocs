6f6a89c82da653cda931803a5f497bc84797024e
rib2333 <rib2333@users.noreply.github.com>
2023-01-18 06:42:54 +0000

    deploy: 25ead151516d0a44221f04f5f3d05c4dfcb73235

---
+++ assets/structures_project-structrue.md.bb44f4b1.js
@@ -0,0 +1,36 @@
+import{_ as s,c as n,o as a,a as p}from"./app.509c06fa.js";const d=JSON.parse('{"title":"总体结构","description":"","frontmatter":{},"headers":[],"relativePath":"structures/project-structrue.md"}'),l={name:"structures/project-structrue.md"},e=p(`<h1 id="总体结构" tabindex="-1">总体结构 <a class="header-anchor" href="#总体结构" aria-hidden="true">#</a></h1><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-palenight"><code><span class="line"><span style="color:#A6ACCD;">├─.nuxt                   nuxt运行时目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─.output                 nuxt打包输出目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">│</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─assets                  静态资源目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─components              组件文件目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─composables             Hooks目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─layouts                 布局文件目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─middleware              路由中间件目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─pages                   页面目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─plugins                 插件目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─public                  公开资源目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─server                  服务器文件目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">│  ├─api                  API接口目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">│  ├─middleware           服务器中间件目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">│  └─router               服务器路由目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─utils                   工具函数目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">│</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─app.config.ts           nuxt应用运行时参数</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─nuxt.config.ts          nuxt应用配置文件</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─tsconfig.json           ts配置文件</span></span>
+<span class="line"><span style="color:#A6ACCD;">│</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─content                 文档目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">│  ├─docs                 项目文档</span></span>
+<span class="line"><span style="color:#A6ACCD;">│     ├─svg               文档使用的svg目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">│     │  └─...</span></span>
+<span class="line"><span style="color:#A6ACCD;">│     └─项目简介.md</span></span>
+<span class="line"><span style="color:#A6ACCD;">│</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─node_modules            依赖文件目录</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─.gitignore              git忽略文件</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─.markdownlint.yaml      markdown格式化配置文件</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─.npmrc                  npm配置文件</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─LICENSE                 开源协议GPL-2</span></span>
+<span class="line"><span style="color:#A6ACCD;">├─package.json            </span></span>
+<span class="line"><span style="color:#A6ACCD;">├─pnpm-lock.yaml          </span></span>
+<span class="line"><span style="color:#A6ACCD;">└─README.md               </span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,2),o=[e];function c(t,r,A,C,i,y){return a(),n("div",null,o)}const u=s(l,[["render",c]]);export{d as __pageData,u as default};
